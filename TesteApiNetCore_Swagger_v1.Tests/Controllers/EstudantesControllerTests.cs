4f4c52971b26583af30356d4f2e392fb947a8085
kelvyn14 <kelvynra@gmail.com>
2022-12-20 20:39:21 -0300

    Inclusão de teste ObterListagemDeUsuariosRealComSucesso e alteração no teste ObterListagemDeUsuariosFakeComSucesso

--- TesteApiNetCore_Swagger_v1.Tests/Controllers/EstudantesControllerTests.cs
+++ TesteApiNetCore_Swagger_v1.Tests/Controllers/EstudantesControllerTests.cs
@@ -1,10 +1,8 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Moq;
 using TesteApiNetCore_Swagger_v1.Controllers;
 using TesteApiNetCore_Swagger_v1.IServices;
 using TesteApiNetCore_Swagger_v1.Service;
-using Xunit;
 using Xunit.Abstractions;
 
 namespace TesteApiNetCore_Swagger_v1.Tests.Controllers
@@ -12,15 +10,21 @@ namespace TesteApiNetCore_Swagger_v1.Tests.Controllers
     public class EstudantesControllerTests
     {
         private readonly ITestOutputHelper _testOutputHelper;
+        private readonly IEstudanteService? _estudanteService;
 
         public EstudantesControllerTests(ITestOutputHelper testOutputHelper)
         {
+            var servico = new ServiceCollection();
+            servico.AddTransient<IEstudanteService, EstudanteService>();
+            var provedor = servico.BuildServiceProvider();
+            _estudanteService = provedor.GetService<IEstudanteService>();
+
             _testOutputHelper = testOutputHelper;
             _testOutputHelper.WriteLine("Iniciando os testes");
         }
 
         [Fact]
-        public void ObterListagemDeUsuariosComSucesso()
+        public void ObterListagemDeUsuariosFakeComSucesso()
         {
             // Arrange
             var expectedResult = 5;
@@ -39,7 +43,22 @@ namespace TesteApiNetCore_Swagger_v1.Tests.Controllers
             var result = controller.Get();
 
             //Assert
-            Assert.Equal(result.Count, expectedResult);
+            Assert.Equal(expectedResult, result.Count);
+        }
+
+        [Fact]
+        public void ObterListagemDeUsuariosRealComSucesso()
+        {
+            // Arrange
+            var expectedResult = 10;
+
+            var controller = new EstudantesController(_estudanteService);
+
+            // Act
+            var result = controller.Get();
+
+            //Assert
+            Assert.Equal(expectedResult, result.Count);
         }
     }
 }
